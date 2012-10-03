d3e2313f891c46410448e1792becbe4cd0129d21
Daniel <danhamilt1@msn.com>
2012-10-03 13:52:09 +0100

    Comment added about std::cout

--- joshcpp/hellodan.cpp
+++ joshcpp/hellodan.cpp
@@ -14,6 +14,10 @@ using namespace std;
  * 
  */
 int main() {
+    
+    //so here you dont actually need the std:: as you are already using the 
+    //namespace std which accounts for the whole file I think
+    
     std::cout << "Hello Daniel\n";	//use cout from the stdlib to print a msg
     return 0;
 }
