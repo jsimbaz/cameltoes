a32bf77a9f75fe21f26679252f859d2f22f32ec8
josh <josh@ubuntu12>
2012-10-03 11:37:13 +0100

    hello world mother fuckers

---
+++ joshcpp/hellodan.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: josh
+ *
+ * Created on 03 October 2012, 11:17
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    std::cout << "Hello Daniel\n";	//use cout from the stdlib to print a msg
+    return 0;
+}
+
