072f163555c795b3b89f6034fef5f24818f79e2e
Daniel <danhamilt1@msn.com>
2012-10-03 14:00:59 +0100

    Changed output to "Hello Dave"

--- joshcpp/hellodan.cpp
+++ joshcpp/hellodan.cpp
@@ -18,7 +18,7 @@ int main() {
     //so here you dont actually need the std:: as you are already using the 
     //namespace std which accounts for the whole file I think
     
-    std::cout << "Hello Daniel\n";	//use cout from the stdlib to print a msg
+    std::cout << "Hello Dave\n";	//use cout from the stdlib to print a msg
     return 0;
 }
 
