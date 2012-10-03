9f92c4bc8ac2e8610218175213efc60c88ce45fe
josh <josh@ubuntu12>
2012-10-03 11:22:17 +0100

    hello world mother fuckers

---
+++ joshcpp/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: josh
+ *
+ * Created on 03 October 2012, 11:17
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    std::cout << "Hello World\n";	//print hello world message
+    return 0;
+}
+
