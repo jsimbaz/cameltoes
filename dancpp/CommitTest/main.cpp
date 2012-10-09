697d537ee0ae7c1c1f46ee983355572a8ef06ed0
Daniel <danhamilt1@msn.com>
2012-10-09 20:54:11 +0100

    Changed COLOR_PUBE to GINGER_PUBES

---
+++ dancpp/CommitTest/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Daniel
+ *
+ * Created on 09 October 2012, 20:54
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
