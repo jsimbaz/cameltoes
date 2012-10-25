be0649115bb7e9a267e2d46e52ce3b564bd39636
Daniel <danhamilt1@msn.com>
2012-10-25 12:43:36 +0100

    Java

--- dancpp/DataStructuresAndAlgorithms/src/datastructuresandalgorithms/ExchangeSort.java
+++ dancpp/DataStructuresAndAlgorithms/src/datastructuresandalgorithms/ExchangeSort.java
@@ -1,60 +1,29 @@
-//Program Description: Basic procedural program as an example of a bubble/exchange sort
-//Program Creator: Daniel Hamilton
+////////////////////////////////////////////////////////////////////
+//Program Description: Uses the exchange/binary sort algorithm    //
+//to sort a list of 10 integers.                                  //
+//Program Creator: Daniel Hamilton                                //  
+//Date Created: 25-Oct-2012                                       //  
+////////////////////////////////////////////////////////////////////
 package datastructuresandalgorithms;
-
-import java.util.Scanner;
 
 public class ExchangeSort {
 
-    public static void main(String[] args) {
+    private static boolean swapped = true;
+    private static int swap;
 
-        int[] numberList = new int[10];
-        String unSorted;
-        Scanner scan = new Scanner(System.in);
+    public static int[] exchangeSort(int[] numberList) {
 
-        //Recieve 10 numbers for sort
-        do {
-
-            System.out.print("Please input 10 numbers (0-9): ");
-            unSorted = scan.nextLine();
-            if (unSorted.length() != 10) {
-                System.out.println("List needs to be length of 10!\n\n");
-            }
-        } while (unSorted.length() != 10);
-
-        //Probably over-complicated string to int converter
-        for (int i = 0; i < 10; i++) {
-            Character tempChar;
-            String tempString;
-            tempChar = unSorted.charAt(i);
-            tempString = tempChar.toString();
-            numberList[i] = Integer.parseInt(tempString);
-        }
-
-        sort(numberList);
-
-        //End sort and print result
-        System.out.print("Result: ");
-        for (int i = 0; i < 10; i++) {
-            System.out.print(numberList[i]);
-        }
-        System.out.println("");
-
-
-    }
-
-    //Exchange sorting method
-    private static void sort(int[] numberList) {
-        boolean swapped = true;
 
         //Start exchange sort
         while (swapped == true) {
+
             swapped = false;
 
             for (int i = 0; i < numberList.length - 1; i++) {
 
                 if (numberList[i] > numberList[i + 1]) {
-                    int swap = numberList[i];
+
+                    swap = numberList[i];
                     numberList[i] = numberList[i + 1];
                     numberList[i + 1] = swap;
 
@@ -63,5 +32,8 @@ public class ExchangeSort {
             }
 
         }
+
+        return numberList;
+
     }
 }
